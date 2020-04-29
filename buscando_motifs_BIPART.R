224625768515bdd2406bc3ea9bece965fe3e11b3
Fred Windsor <31346896+fmwindsor@users.noreply.github.com>
2020-04-29 18:29:19 +0100

    Updates

--- buscando_motifs_BIPART.R
+++ buscando_motifs_BIPART.R
@@ -14,8 +14,7 @@ head(INT)
 ####Interactions soybean-insects
 polin<-function(INT){
 			level.superior<-NULL
-			por.fila<-length(INT[,3])
-			for(i in 1:por.fila){
+			for(i in 1:length(INT[,3])){
 					if (INT[i,"lower.taxon"]=="pl1"){
       				up.tax<- INT[i,"upper.taxon"]
 					level.superior[i]<-as.character(up.tax)
@@ -32,12 +31,12 @@ poli_en_motif1<-data.frame(poli_en_motif2[poli_en_motif2$polin.INT != "no",])#me
 colnames(poli_en_motif1)<-c("low.level","high.level","weight")
 poli_en_motif1
 
+
 ####Detecting plants from the edges that are participating on motif.  
 ###Insect-plant from the edge
 planta.edge<-function(INT,k){
 	level.inferior<-NULL
-	por.fila<-length(INT[,1])
-	for(i in 1:por.fila){
+	for(i in 1:length(INT[,1])){
 		if (INT[i,"upper.taxon"]==as.character(k)){ 
     		low.tax<- INT[i,"lower.taxon"]
 		level.inferior[i]<-as.character(low.tax)
@@ -53,11 +52,8 @@ amigos<-as.character(poli_en_motif1[,2])#insects participando de motifs
 
 #function including planta.edge function (for each partner insect in soybea crop...)
 total1<-function(INT,amigos){
-	filas<-nrow(INT)
-	columnas<-length(amigos)
-	final<-matrix(,filas,columnas)
-	uno.a.uno<-length(amigos)
-	for (j in 1:uno.a.uno){ #para cada companero de soja
+	final<-matrix(NA,nrow(INT),length(amigos))
+	for (j in 1:length(amigos)){ #para cada companero de soja
 		k<-amigos[j]
 		final[,j]<-planta.edge(INT,k)#busco sus plantas compa;eras de borde
 	}
@@ -90,7 +86,7 @@ elementos<-length(listado_motifs)
 		}
 	}
 
-listado_motifs2
+listado_motifs2_test <- as.matrix(do.call(rbind, listado_motifs2)) # this merges the separate edgelists into one
 
 ####"listado_motifs2" is a list of interactions participating in motifs 
 ####if output is "NULL", NO MOTIF FOUND
